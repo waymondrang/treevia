202fcaf844c63dce19254882a800e59092fd26f8
Annabella Macaluso <amacalus@ucsd.edu>
2023-02-21 11:05:46 -0800

    Updates to Trivia Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Trivia from './Trivia.js';
@@ -8,13 +7,16 @@ import Trivia from './Trivia.js';
 function App() {
   let state = {
     question: {
-      question: "Example Question: How do nitrates affect soil?",
-      wrongAnswer1: "12",
-      wrongAnswer2: "13",
-      wrongAnswer3: "15",
-      answer: "27"
+      question: "Drip irrigation is an efficient water method that delivers water directly to the plant’s roots. By how many percent does drip irrigation reduce water usage compared to surface irrigation?",
+      wrongAnswer1: "Kelp meal",
+      wrongAnswer2: "Manure",
+      wrongAnswer3: "Compost",
+      answer: "Ammonium phosphate",
+      explanation: "Natural fertilizers are organic substances that contain vital nutrients for plant growth whereas synthetic fertilizers are made of synthesized chemicals like nitrogen, potassium and phosphorus (Hazra). "
     },
   };
+
+  
   return (
     
     <Trivia questionData={state.question}/>
